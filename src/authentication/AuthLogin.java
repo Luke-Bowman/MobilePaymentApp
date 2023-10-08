e4273faad708be8931b2c9261c33d1219b8e4740
fquezado <fquezado18@gmail.com>
2023-10-07 21:25:16 -0400

    added changes

---
+++ src/authentication/AuthLogin.java
@@ -0,0 +1,16 @@
+package authentication;
+
+public class AuthLogin {
+    private AuthService authService;
+
+    public AuthLogin(AuthService authService) {
+        this.authService = authService;
+    }
+
+    public boolean loginUser(String username, String password) {
+        // Stub: Handle user login
+        System.out.println("Logging in user: " + username);
+        return authService.validateCredentials(username, password);
+    }
+}
+
